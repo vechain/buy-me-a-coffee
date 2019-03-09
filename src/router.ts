8aa081bf3e14734125e9562602787a0e07f988ee
tony <tony.li@vechain.com>
2019-03-09 21:19:38 +0800

    set mode to history

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Backer from './views/Backer.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    },
     {
       path: '/backer',
       name: 'Backer',
