17c6ee4c05afde7c3f87d12dea431796f80444fc
tony <tony.li@vechain.com>
2019-03-21 17:13:53 +0800

    add analytics

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 import './style.scss'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+Vue.use(VueAnalytics, {
+  id: 'UA-132391998-2',
+  disabled: process.env.NODE_ENV !== 'production',
+})
 
 new Vue({
   router,
